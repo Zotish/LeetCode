63670c8e1264dc1b86ae436fca2817e7197de85b
Zotish Chandra <72625524+Zotish@users.noreply.github.com>
2023-10-06 22:31:54 +0600

    Time: 0 ms (100%), Space: 3.6 MB (11.86%) - LeetHub

---
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.go
@@ -0,0 +1,9 @@
+func prefixCount(words []string, pref string) int {
+    count:=0
+    for _,val:= range words{
+        if strings.HasPrefix(val,pref){
+            count++
+        }
+    }
+    return count
+}
