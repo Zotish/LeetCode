12f93c4759d92a13c9108defe6188922938eaa01
Zotish Chandra <72625524+Zotish@users.noreply.github.com>
2023-10-08 22:58:53 +0600

    Time: 2 ms (77.37%), Space: 2.6 MB (59.31%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.go
+++ 0206-reverse-linked-list/0206-reverse-linked-list.go
@@ -9,7 +9,7 @@ func reverseList(head *ListNode) *ListNode {
     var prev *ListNode
     current := head
     for current != nil {
-        nextTemp := current.Next // store next node
+        nextTemp:= current.Next // store next node
         current.Next = prev      // reverse current node's direction
         prev = current           // move prev to current node
         current = nextTemp       // move to the next node
