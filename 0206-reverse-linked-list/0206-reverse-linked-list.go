9a8c87610ce8985a49db1d2f840a5b6924ec821a
Zotish Chandra <72625524+Zotish@users.noreply.github.com>
2023-10-08 22:55:16 +0600

    Time: 2 ms (77.37%), Space: 2.5 MB (59.31%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    current := head
+    for current != nil {
+        nextTemp := current.Next // store next node
+        current.Next = prev      // reverse current node's direction
+        prev = current           // move prev to current node
+        current = nextTemp       // move to the next node
+    }
+    return prev
+}
