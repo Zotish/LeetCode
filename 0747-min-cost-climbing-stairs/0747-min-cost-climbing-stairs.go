66a5ce684e9e8ffaf8805fe7abd176092f7019fa
Zotish Chandra <72625524+Zotish@users.noreply.github.com>
2023-10-30 13:53:40 +0600

    Time: 0 ms (100%), Space: 2.8 MB (99.93%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.go
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.go
@@ -1,20 +1,17 @@
 func minCostClimbingStairs(cost []int) int {
-n:=len(cost)
-dp:=make([]int,n+1)
-  for i:=0;i<len(cost);i++{
-      dp[i]=-1
-  }
-  dp[0]=cost[0]
-  dp[1]=cost[1]
-  for i:=2;i<n;i++{
-     left:=cost[i]+dp[i-1]
-     right:=cost[i]+dp[i-2]
-     dp[i]=min(left,right)
-  }
-  return min(dp[n-1],dp[n-2])
+    n := len(cost)
+    prev, prev2 := cost[0], cost[1]
+    
+    for i := 2; i < n; i++ {
+        curri := cost[i] + min(prev, prev2)
+        prev, prev2 = prev2, curri
+    }
+    
+    return min(prev, prev2)
 }
-func min(a,b int) int{
-    if a>b{
+
+func min(a, b int) int {
+    if a > b {
         return b
     }
     return a
